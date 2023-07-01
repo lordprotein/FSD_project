d23d222889d0a6cb8959efa724dae93b4e189804
lordprotein <gamee.logi@gmail.com>
2023-07-02 01:57:21 +0600

    update page "login"

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,2 +1,16 @@
-const LoginPage = () => null
+import React from 'react'
+
+import { LoginWidget } from 'widgets/login'
+
+import { ToggleAuthProcesses } from 'features/auth'
+
+import { LoginPageStyled } from './styled'
+
+const LoginPage = () => (
+  <LoginPageStyled>
+    <ToggleAuthProcesses />
+
+    <LoginWidget />
+  </LoginPageStyled>
+)
 export default LoginPage
---
+++ src/pages/login/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const LoginPageStyled = styled.main`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  row-gap: 40px;
+`
