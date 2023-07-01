e2db8bb4cb333392280e59bcc70b468dcde417f2
lordprotein <gamee.logi@gmail.com>
2023-07-02 01:58:10 +0600

    update page "sign-up"

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,3 +1,17 @@
-const SignUpPage = () => null
+import React from 'react'
+
+import { SignUpWidget } from 'widgets/sign-up'
+
+import { ToggleAuthProcesses } from 'features/auth'
+
+import { SignUpPageStyled } from './styled'
+
+const SignUpPage = () => (
+  <SignUpPageStyled>
+    <ToggleAuthProcesses />
+
+    <SignUpWidget />
+  </SignUpPageStyled>
+)
 
 export default SignUpPage
