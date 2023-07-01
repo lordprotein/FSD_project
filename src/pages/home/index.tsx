3872535483c065d2fa42d5c7735d78498e7b3f57
lordprotein <gamee.logi@gmail.com>
2023-07-02 01:55:29 +0600

    added widget "AdvertisingCard" to page "home"

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,14 @@
-const HomePage = () => null
+import React from 'react'
+
+import { AdvertisingCard } from 'widgets/advertising'
+
+const HomePage = () => (
+  <AdvertisingCard
+    subtitle="Subtitle"
+    timeBeforeStart="asd"
+    title="title home"
+    onClickReadButton={() => console.log('new widget hello!')}
+  />
+)
 
 export default HomePage
